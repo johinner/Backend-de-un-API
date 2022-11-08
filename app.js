4e49ace2c7cf5cd387a866419c8a03760ee2e47d
johinner <johimadacor@live.com>
2022-11-07 22:02:56 -0500

    archivo Readme

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Hello Word')
+})
 
 app.listen(PORT, () => {
     console.log('Server starte at port '+ PORT)
